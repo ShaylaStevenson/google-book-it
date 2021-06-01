80f979ebe4f5c09dbfd3cb28352db75301025e51
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-06-01 16:36:28 -0700

    build

---
+++ client/build/precache-manifest.659f7183cb4267b38c3e2c67057a7a3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f16b1703f29e106b1d8917cb283ebf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6d5e0f58a52f99dc5df",
+    "url": "/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "6f19c3891bf79255b084",
+    "url": "/static/css/main.7ac7d2be.chunk.css"
+  },
+  {
+    "revision": "e6d5e0f58a52f99dc5df",
+    "url": "/static/js/2.584e70bf.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.584e70bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f19c3891bf79255b084",
+    "url": "/static/js/main.f6f98fce.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
